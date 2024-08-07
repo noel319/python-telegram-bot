cbfa797804f709605d8acf026c21ce42dcfced45
Alexander Zeisl <azeisl917@gmail.com>
2024-08-07 17:10:50 +0300

    add inline buttons

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -4,9 +4,10 @@ from aiogram import Dispatcher
 from .start import router as start_router
 from .help import router as help_router
 from .echo import router as echo_router
-
+from .inline_buttons import router as inline_buttons_router
 
 def register_handlers(dp: Dispatcher):
     dp.include_router(start_router)
     dp.include_router(help_router)
     dp.include_router(echo_router)
+    dp.include_router(inline_buttons_router)
--- src/handlers/inline_buttons.py
+++ src/handlers/inline_buttons.py
@@ -1,8 +1,8 @@
-# handlers/start.py
+# handlers/inline_buttons.py
 
 from aiogram import types, Router
-from aiogram.filters import Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
 
 router = Router()
 
@@ -13,3 +13,12 @@ async def send_welcome(message: types.Message):
         [InlineKeyboardButton(text="Choice 2", callback_data="choice_2")]
     ])
     await message.answer("Welcome to our bot! Please choose an option:", reply_markup=keyboard)
+
+@router.callback_query(lambda c: c.data in ["choice_1", "choice_2"])
+async def process_callback(callback_query: types.CallbackQuery):
+    choice = callback_query.data
+    if choice == "choice_1":
+        await callback_query.message.answer("You have selected Choice 1")
+    elif choice == "choice_2":
+        await callback_query.message.answer("You have selected Choice 2")
+    await callback_query.answer()  # Acknowledge the callback query
